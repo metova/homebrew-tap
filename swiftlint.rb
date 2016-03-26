b02dead85c886be38bc4f1348581456d8b16fbb8
Chris Dillard <chris.dillard@metova.com>
2016-03-26 16:20:03 -0600

    Fix to Metova Swxftlint.

---
+++ swiftlint.rb
@@ -0,0 +1,17 @@
+class Swiftlint < Formula
+  desc "Experimental tool to enforce Swift style and conventions"
+  homepage "https://github.com/metova/SwiftLint"
+  url "https://github.com/metova/SwiftLint.git", :tag => "0.9.2m1", :revision => "11e59cb7174c3f540b55d98e8051ca477a86f654"
+  head "https://github.com/metova/SwiftLint.git"
+
+  depends_on :xcode => ["7.1", :build]
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
+  end
+
+  test do
+    (testpath/"Test.swift").write "import Foundation\n"
+    system "#{bin}/swiftlint"
+  end
+end
