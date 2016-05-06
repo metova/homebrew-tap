2fb98a945dfd690898d3c8fcc565526eab0b5eef
Chris Dillard <chris.dillard@metova.com>
2016-05-06 11:35:20 -0600

    bump version

--- swiftlint.rb
+++ swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/metova/SwiftLint"
-  url "https://github.com/metova/SwiftLint.git", :tag => "0.10.0m1", :revision => "8d3be6546d580d63bc2f294baddc47e091774906"
+  url "https://github.com/metova/SwiftLint.git", :tag => "0.10.0m2", :revision => "c1e021242b164bd01e41db47799db9b9b656a5c4"
   head "https://github.com/metova/SwiftLint.git"
 
   depends_on :xcode => ["7.1", :build]
